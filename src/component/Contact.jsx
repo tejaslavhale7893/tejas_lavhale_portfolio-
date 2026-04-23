59b9d9ea61bc98997d859a913a5c3d6175eff8fc
tejaslavhale7893 <tejaslavhale7893@gmail.com>
2026-04-23 11:18:50 +0530

    fix: emailjs error solved

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser'
 import { FaWhatsapp, FaInstagram, FaLinkedin, FaFacebook, FaEnvelope } from "react-icons/fa";
 import { useLanguage } from '../context/LanguageContext';
 
